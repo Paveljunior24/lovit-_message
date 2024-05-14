4d126bb6ba4b18737a5b5579b619f254d46c322f
Paveljunior24 <56825004+Paveljunior24@users.noreply.github.com>
2024-05-14 12:35:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db' 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
+
+# Configuration de la base de données
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'  # Chemin de la base de données SQLite
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
+# Définition du modèle de la base de données
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.String(200), nullable=True)
@@ -15,20 +19,30 @@ class Message(db.Model):
     created_at = db.Column(db.DateTime(), default=datetime.utcnow)
 
 
-@app.route("/<name>", methods= ['GET', 'POST'])
+# Route principale de l'application
+@app.route("/<name>", methods=['GET', 'POST'])
 def start_page(name):
     if request.method == 'POST':
-        new_message =Message(
-            user = name,
-            content = request.form['content']
+        # Si une requête POST est reçue, enregistrer un nouveau message dans la base de données
+        new_message = Message(
+            user=name,
+            content=request.form['content']
         )
         db.session.add(new_message)
         db.session.commit()
+
+    # Récupération de tous les messages de la base de données et les trier par date de création
     messages = Message.query.order_by(Message.created_at).all()
-    return render_template('index.html' , messages=messages , name=name)
+    
+    # Rendu de la page HTML avec les messages récupérés et le nom passé en paramètre
+    return render_template('index.html', messages=messages, name=name)
 
 
+# Point d'entrée principal de l'application Flask
 if __name__ == "__main__":
     with app.app_context():
+        # Création de toutes les tables de la base de données si elles n'existent pas encore
         db.create_all()
+        
+        # Lancement de l'application en mode débogage
         app.run(debug=True)
